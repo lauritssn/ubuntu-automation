3521229aab3c7a997e0af2f2bfe0bb5f38f513ae
Laurits Søgaard Nielsen <post@lauritsnielsen.dk>
2025-07-22 10:40:27 +0200

    Fix

--- subscripts/dokku_install.sh
+++ subscripts/dokku_install.sh
@@ -5,13 +5,24 @@
 
 show_info "Installing Dokku..."
 
-# Download and install Dokku bootstrap script (latest stable)
-wget -NP . https://dokku.com/install/v0.35.22/bootstrap.sh
-DOKKU_TAG=v0.35.22 bash bootstrap.sh
+# Download the installation script
+wget -NP . https://dokku.com/bootstrap.sh
+
+# Run the installer
+sudo DOKKU_TAG=v0.35.20 bash bootstrap.sh
 
 # Check if installation was successful
 if [ $? -eq 0 ]; then
     show_warn "Dokku installation completed successfully"
+
+    # Configure server domain if EMAIL_DOMAIN is available
+    if [ -n "$EMAIL_DOMAIN" ]; then
+        show_info "Configuring Dokku global domain to: $EMAIL_DOMAIN"
+        dokku domains:set-global "$EMAIL_DOMAIN"
+    else
+        show_info "Configuring Dokku global domain to default: dokku.me"
+        dokku domains:set-global dokku.me
+    fi
 else
     show_err "Dokku installation failed"
     exit 1
